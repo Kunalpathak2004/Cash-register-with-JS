110ed4f3b9d2696b5221c0b4fb6c21d14e1c1b6d
Kavita Pathak <110056559+Kunalpathak2004@users.noreply.github.com>
2022-10-22 10:26:31 +0530

    feat-added more JS

--- app.js
+++ app.js
@@ -2,25 +2,34 @@ var billAmount = document.querySelector("#bill-amount");
 var checkButton = document.querySelector("#check-button");
 var cashGiven = document.querySelector("#cash-given");
 var message = document.querySelector("#error-msg");
-checkButton.addEventListener("click" , function validateBillAndCashAmount () {
-    message.style.display="none";
-    if(billAmount>0){
-           if(cashGiven ≥ billAmount){
-                 var amountToBEReturned= cashGiven.value - billAmount.value ;
-                 calculateChange(amountToBEReturned);
-           }
-           else{
-            erroeMessage("Cash given should atleast be equal to bill amount");
-           }
-    };
-    else{
-          erroeMessage("Invalid Bill Amount");
+
+checkButton.addEventListener("click", validateBillAndCashAmount);
+
+function validateBillAndCashAmount() {
+    hideMessage ();
+    console.log(billAmount.value);
+    if (billAmount > 0) {
+        if (cashGiven >= billAmount) {
+            var amountToBeReturned = cashGiven.value - billAmount.value;
+            calculateChange(amountToBeReturned);
+        } else {
+            errorMessage("Cash given should atleast be equal to bill amount");
+        }
     }
-});
+    else {
+        errorMessage("Invalid Bill Amount");
+    }
+};
 
-function calculateChange() {}
+ function calculateChange() {
 
-function erroeMessage(message) {
-    message.style.display="block";
-    message.innerText= message
+ }
+
+function hideMessage() {
+    message.style.display = "none";
+}
+
+function errorMessage(msg) {
+    message.style.display = "block";
+    message.innerText = msg;
 }
