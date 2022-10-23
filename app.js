3ba778e96aacf649432013d6442c60ca3cf7b1c2
Kavita Pathak <110056559+Kunalpathak2004@users.noreply.github.com>
2022-10-23 13:59:51 +0530

    feat-css updates

--- app.js
+++ app.js
@@ -2,14 +2,19 @@ var billAmount = document.querySelector("#bill-amount");
 var checkButton = document.querySelector("#check-button");
 var cashGiven = document.querySelector("#cash-given");
 var message = document.querySelector("#error-msg");
+var nextButton = document.querySelector("#nxt-btn");
+var noOfNotes = document.querySelectorAll(".no-of-notes");
+
+var availableNotes = [2000 , 500 , 100 , 20 , 10 , 5 , 1]; 
 
 checkButton.addEventListener("click", validateBillAndCashAmount);
 
 function validateBillAndCashAmount() {
     hideMessage ();
-    console.log(billAmount.value);
-    if (billAmount > 0) {
-        if (cashGiven >= billAmount) {
+    
+    if (billAmount.value > 0) {
+        hideCashGiven();
+        if (cashGiven.value >= billAmount.value) {
             var amountToBeReturned = cashGiven.value - billAmount.value;
             calculateChange(amountToBeReturned);
         } else {
@@ -21,7 +26,14 @@ function validateBillAndCashAmount() {
     }
 };
 
- function calculateChange() {
+ function calculateChange(amountToBeReturned) {
+       for(let i=0; i < availableNotes.length; i++){
+        var numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
+        amountToBeReturned %= availableNotes[i];
+        noOfNotes[i].innerText = numberOfNotes;
+       }
+
+
 
  }
 
@@ -32,4 +44,8 @@ function hideMessage() {
 function errorMessage(msg) {
     message.style.display = "block";
     message.innerText = msg;
+}
+
+function hideCashGiven(){
+        cashGiven.style.display = "none";
 }
